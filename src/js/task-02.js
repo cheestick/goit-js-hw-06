ce4fff7fae2e2d731f24677c78931e3e1ea26a4e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-02-03 14:26:00 +0200

    tast-02 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const createIngredientItem = ingredient => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add('item');
+  return ingredientItem;
+};
+
+const allIngredients = ingredients.map(createIngredientItem);
+
+ingredientsListRef.append(...allIngredients);
